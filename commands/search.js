28762f547ca65173a6d66900092fb3133c8bedf5
Jiten Patel <45632589+jitenpatel7@users.noreply.github.com>
2020-05-19 23:02:54 +0100

    feat(jp-18): add untappd search (#23)

---
+++ commands/search.js
@@ -0,0 +1,30 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+const { noSearchResults, untappdHome } = require('../constants/untappd')
+
+module.exports = {
+    name: 'fb',
+    description: 'Find a beer on Untappd *(Search results may vary, but hey-ho)*',
+    args: true,
+    usage: `\`<brewery> <beer>\``,
+    execute(message, args) {
+        const searchBeerUrl = `https://untappd.com/search?q=${args}`
+
+        request(searchBeerUrl, (error, response, html) => {
+            if (response.statusCode === 404) {
+                return;
+            }
+
+            const $ = cheerio.load(html);
+            const beerPage = $('.name a').attr('href');
+            const noBeerResults = $('.results-none p').text();
+
+            if (noBeerResults.includes(noSearchResults)) {
+                return message.channel.send(`${args} returned no results within Untappd`);
+            }
+
+            message.channel.send(`${untappdHome}${beerPage}`);
+        })
+    }
+}
