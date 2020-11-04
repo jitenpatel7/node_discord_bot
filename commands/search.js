47c3a9b2cf1a11acc88647151d20c053c66e3fb5
Jiten Patel <jiten.patel@gamesys.co.uk>
2020-11-04 17:41:18 +0000

    feat(jp-23): dupes v1

--- commands/search.js
+++ commands/search.js
@@ -1,30 +1,30 @@
 const cheerio = require('cheerio');
 const request = require('request');
 
-const { noSearchResults, untappdHome } = require('../constants/untappd')
+const { noSearchResults, untappdHome } = require('../constants/untappd');
 
 module.exports = {
-    name: 'fb',
-    description: 'Find a beer on Untappd *(Search results may vary, but hey-ho)*',
-    args: true,
-    usage: `\`<brewery> <beer>\``,
-    execute(message, args) {
-        const searchBeerUrl = `https://untappd.com/search?q=${args}`
+	name: 'fb',
+	description: 'Find a beer on Untappd *(Search results may vary, but hey-ho)*',
+	args: true,
+	usage: '`<brewery> <beer>`',
+	execute(message, args) {
+		const searchBeerUrl = `https://untappd.com/search?q=${args}`;
 
-        request(searchBeerUrl, (error, response, html) => {
-            if (response.statusCode === 404) {
-                return error;
-            }
+		request(searchBeerUrl, (error, response, html) => {
+			if (response.statusCode === 404) {
+				return error;
+			}
 
-            const $ = cheerio.load(html);
-            const beerPage = $('.name a').attr('href');
-            const noBeerResults = $('.results-none p').text();
+			const $ = cheerio.load(html);
+			const beerPage = $('.name a').attr('href');
+			const noBeerResults = $('.results-none p').text();
 
-            if (noBeerResults.includes(noSearchResults)) {
-                return message.channel.send(`${args} returned no results within Untappd`);
-            }
+			if (noBeerResults.includes(noSearchResults)) {
+				return message.channel.send(`${args} returned no results within Untappd`);
+			}
 
-            message.channel.send(`${untappdHome}${beerPage}`);
-        })
-    }
-}
+			message.channel.send(`${untappdHome}${beerPage}`);
+		});
+	},
+};
